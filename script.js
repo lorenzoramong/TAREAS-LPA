2c19fee70f9ced95d88e06328605763268f00a40
lorenzoramong <lorenzoramong@gmail.com>
2025-11-04 22:02:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL pública del Apps Script (asegurate de que termine en /exec)
-const API_URL = "https://script.google.com/macros/s/AKfycbxmZlkuTJ9NvNKi4eAWUnWvJe7JErKaX6B7ISDkf_D1LHaeE-NwO8gZBqg9Ro0zeKhp_A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyNuN89FmoLohAwlqhaDulV6tNr6dXhkYEFxTo1iK5I09H-trylUGxHoKS-NqE28eCd/exec";
 
 // Manejador del formulario de inicio de sesión
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
